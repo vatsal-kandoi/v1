f55ba97c81f9d6f826cfdbbcb88bfb67bec0470a
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-03-19 17:02:54 +0530

    Added navbar and landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
