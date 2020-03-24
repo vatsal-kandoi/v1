d3212bf9b957606f4ddfbdad0430990516543bfe
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-03-24 20:31:47 +0530

    Updated travis for CNAME

--- create404.js
+++ create404.js
@@ -4,3 +4,4 @@ let read = fs.createReadStream("dist/portfolio/index.html");
 let write = fs.createWriteStream("dist/portfolio/404.html");
 
 read.pipe(write);
+fs.writeFileSync('dist/portfolio/CNAME', 'vatsalkandoi.tech');
